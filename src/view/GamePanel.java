99d2c26e9dcf7cb5446c132461fb0620c2a9adf9
Andres Evans <andres_evans@outlook.com>
2023-08-17 00:19:51 -0400

    Moving rectangle with changing colors

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -7,12 +7,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;//this class will be used in mouseListener and mouseMotionListener
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 0.1f, yDir = 0.1f; //to bounce rectangle from screen edge, number is speed of direction
+    private int frames = 0;
+    private long lastCheck = 0; //check for FPS
+    private Color color = new Color(150,20,90);
+    private Random random;// to randomize color values
 
     public GamePanel() {
+        random = new Random();
         this.mouseInputs = new MouseInputs(this); //initialize the mouse inputs
         addKeyListener(new KeyboardInputs(this));//keyboard inputs are drawn into the panel
         addMouseListener(mouseInputs);// inputs from mouse
@@ -21,18 +28,18 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value){
         this.xDelta += value;
-        repaint(); // repaint the game panel after an input to give impression of movement
+
     }
 
     public void changeYDelta(int value){
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
 
@@ -40,7 +47,44 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g); //calling the superclass of the existing paintComponent
         // this call also cleans the surface to paint a new picture
-        g.fillRect(xDelta, yDelta,200,50); //position and size of the rectangle
+
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int)xDelta, (int) yDelta,200,50); //position and size of the rectangle
+
+        frames ++;
+
+        // Old Game Loop
+        if (System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;//resetting frames for elapsed since last time check
+        }
+
+        repaint(); // repaint the game panel after an input to give impression of movement
+    }
+
+    public void updateRectangle(){
+        xDelta += xDir;
+
+        if(xDelta > 300 || xDelta < 1){ //if value is at the edge of the screen it will reverse direction
+            xDir *= -1;//times negative value to reverse the direction
+            color = getRndColor();
+        }
+
+        yDelta += yDir;
+
+        if(yDelta > 300 || yDelta < 1){
+            yDir *= -1;
+            color = getRndColor();
+        }
+    }
+
+    public Color getRndColor(){
+        int r = random.nextInt(255);//returns between including 0 - 255 as to the values for color parameters
+        int b = random.nextInt(255);
+        int g = random.nextInt(255);
+        return new Color(r,b,g);
     }
 
 
